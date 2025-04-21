fa303c332d29a6b80343b893f1bd7742dd764f20
Felipe Cupitó <65612488+FelipeCupito@users.noreply.github.com>
2025-04-21 13:39:35 -0300

    fixs

--- include/eos_shared_services.hpp
+++ include/eos_shared_services.hpp
@@ -2,21 +2,19 @@
 #include <type_traits>
 #include <utility>
 
-// Base para manejar SharedVar… accesibles por .shared().get<Var>()
 template<typename Peripheral, typename VarEnum>
 class SharedBase {
     Peripheral& ref_;
 public:
     explicit constexpr SharedBase(Peripheral& ref) noexcept : ref_(ref) {}
 
+    // Acceso seguro a la variable v
     template<VarEnum v>
-    decltype(auto) get() {
-        //static_assert(v != VarEnum::COUNT, "VarEnum::COUNT no es válido");
+    decltype(auto) get() {        
         return Peripheral::template access_shared<v>(ref_);
     }
 };
 
-// Base para invocar servicios type‑safe por .services().call<Svc>(…)
 template<typename Peripheral, typename SvcEnum>
 class ServicesBase {
     Peripheral& ref_;
@@ -25,7 +23,6 @@ public:
 
     template<SvcEnum s, typename... Args>
     void call(Args&&... args) {
-        //static_assert(s != SvcEnum::COUNT, "SvcEnum::COUNT no es válido");
         Peripheral::template invoke_service<s>(ref_, std::forward<Args>(args)...);
     }
 };
